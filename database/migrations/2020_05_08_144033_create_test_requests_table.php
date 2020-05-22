0e31df2ff899ca46a0f866612c633c515a5bef09
Monirul Alom <monirulalom@gmail.com>
2020-05-22 11:20:06 +0600

    initial commit

---
+++ database/migrations/2020_05_08_144033_create_test_requests_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTestRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('test_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('district');
+            $table->string('upazilla');
+            $table->string('village');
+            $table->string('email');
+            $table->string('mobileno');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('test_requests');
+    }
+}
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,114 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name') }} {{ app()->version() }}</title>
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    <div id="app">
+        <nav class="navbar has-shadow">
+            <div class="container">
+                <div class="navbar-brand">
+                    <a class="navbar-item" href="{{url('/')}}">
+                        <img src="{{asset('images/logo.png')}}">
+                        <span class="site-title">{{config('app.name')}}</span>
+                    </a>
+
+
+                    <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false"
+                        data-target="navbarBasicExample">
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                        <span aria-hidden="true"></span>
+                    </a>
+                </div>
+
+                <div id="navbarBasicExample" class="navbar-menu">
+                    <div class="navbar-start">
+                        <a class="navbar-item">
+                            Home
+                        </a>
+
+                        <a class="navbar-item">
+                            Request for Test
+                        </a>
+                        <a class="navbar-item">
+                            Awareness Quiz
+                        </a>
+
+                        <div class="navbar-item has-dropdown is-hoverable">
+                            <a class="navbar-link">
+                                More
+                            </a>
+
+                            <div class="navbar-dropdown">
+                                <a class="navbar-item">
+                                    About
+                                </a>
+                                <a class="navbar-item">
+                                    Contact
+                                </a>
+                                <hr class="navbar-divider">
+                                <a class="navbar-item">
+                                    Report an issue
+                                </a>~
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="navbar-menu" id="navMenu">
+                        <div class="navbar-start"></div>
+
+                        <div class="navbar-end">
+                            @if (Auth::guest())
+                            <div class="navbar-end">
+                                <div class="navbar-item">
+                                    <div class="buttons">
+                                        <a class="button is-primary" href="{{ url('/register') }}">
+                                            <strong>Sign up</strong>
+                                        </a>
+                                        <a class="button is-light" href="{{url('/login')}}">
+                                            Log in
+                                        </a>
+                                    </div>
+                                </div>
+                                @else
+                                <div class="navbar-item has-dropdown is-hoverable">
+                                    <a class="navbar-link" href="#">{{ Auth::user()->name }}</a>
+
+                                    <div class="navbar-dropdown">
+                                        <a class="navbar-item" href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                            style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </div>
+                                </div>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
+        </nav>
+        @yield('content')
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+
+</html>
